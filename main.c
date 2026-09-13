53d423872723a15a13e63f907bd52f62e2ba46f8
Jai Pandey <jaipandey743@gmail.com>
2026-09-13 07:44:36 +0530

    feat: add browser support

--- main.c
+++ main.c
@@ -1,11 +1,19 @@
 #include <raylib.h>
 #include "menu.h"
+#ifdef __EMSCRIPTEN__
+#include <emscripten/emscripten.h>
+#endif
 #define MAX 15
 typedef struct { float x, y, speed; } Enemy;
 
-static void ResetGame(float *px, float *pscore, Enemy e[]) {
-    *px = 870;
-    *pscore = 0;
+// globals so UpdateDrawFrame(void) can see them on web
+static float x = 870, y = 880, speed = 300, score = 0;
+static Enemy e[MAX];
+static State state = MENU;
+
+static void ResetGame(void) {
+    x = 870;
+    score = 0;
     for (int i = 0; i < MAX; i++) {
         e[i].x = (float)GetRandomValue(0, 1880);
         e[i].y = -40 - i * 250;
@@ -13,65 +21,74 @@ static void ResetGame(float *px, float *pscore, Enemy e[]) {
     }
 }
 
+static void UpdateDrawFrame(void) {
+    float dt = GetFrameTime();
+    if (state == MENU) {
+        int action = MenuClick();
+        if (action == 1) {
+            ResetGame();
+            state = PLAYING;
+        } else if (action == 2) {
+            UnloadMenuFont();
+            CloseWindow();
+#ifdef __EMSCRIPTEN__
+            emscripten_cancel_main_loop();
+#endif
+            return;
+        }
+        BeginDrawing();
+        DrawMenu();
+        EndDrawing();
+        return;
+    }
+    if (state == GAMEOVER) {
+        if (GameOverDone()) state = MENU;
+        BeginDrawing();
+        DrawGameOver(score);
+        EndDrawing();
+        return;
+    }
+    if (IsKeyDown(KEY_RIGHT)) x += speed * dt;
+    if (IsKeyDown(KEY_LEFT)) x -= speed * dt;
+    score += 5 * dt;
+    for (int i = 0; i < MAX; i++) {
+        e[i].y += e[i].speed * dt;
+        if (e[i].y > 1080) {
+            e[i].y = -40;
+            e[i].x = (float)GetRandomValue(0, 1880);
+        }
+    }
+    Rectangle player = { x, y, 40, 40 };
+    for (int i = 0; i < MAX; i++) {
+        Rectangle er = { e[i].x, e[i].y, 40, 40 };
+        if (CheckCollisionRecs(player, er)) {
+            state = GAMEOVER;
+            break;
+        }
+    }
+    if (state != PLAYING) return;
+    BeginDrawing();
+    ClearBackground(BLACK);
+    const char *stext = TextFormat("%d", (int)score);
+    int sw = MeasureText(stext, 50);
+    DrawText(stext, 1920 - sw - 60, 30, 50, GRAY);
+    DrawRectangle((int)x, (int)y, 40, 40, WHITE);
+    for (int i = 0; i < MAX; i++) {
+        DrawRectangle((int)e[i].x, (int)e[i].y, 40, 40, RED);
+    }
+    EndDrawing();
+}
+
 int main(void) {
     InitWindow(1920, 1080, "shark");
     LoadMenuFont();
-    float x = 870, y = 880, speed = 300, score = 0;
-    Enemy e[MAX];
-    ResetGame(&x, &score, e);
-    State state = MENU;
-    while (!WindowShouldClose()) {
-        float dt = GetFrameTime();
-        if (state == MENU) {
-            int action = MenuClick();
-            if (action == 1) {
-                ResetGame(&x, &score, e);
-                state = PLAYING;
-            } else if (action == 2) {
-                break;
-            }
-            BeginDrawing();
-            DrawMenu();
-            EndDrawing();
-            continue;
-        }
-        if (state == GAMEOVER) {
-            if (GameOverDone()) state = MENU;
-            BeginDrawing();
-            DrawGameOver(score);
-            EndDrawing();
-            continue;
-        }
-        if (IsKeyDown(KEY_RIGHT)) x += speed * dt;
-        if (IsKeyDown(KEY_LEFT)) x -= speed * dt;
-        score += 5 * dt;
-        for (int i = 0; i < MAX; i++) {
-            e[i].y += e[i].speed * dt;
-            if (e[i].y > 1080) {
-                e[i].y = -40;
-                e[i].x = (float)GetRandomValue(0, 1880);
-            }
-        }
-        Rectangle player = { x, y, 40, 40 };
-        for (int i = 0; i < MAX; i++) {
-            Rectangle er = { e[i].x, e[i].y, 40, 40 };
-            if (CheckCollisionRecs(player, er)) {
-                state = GAMEOVER;
-                break;
-            }
-        }
-        if (state != PLAYING) continue;
-        BeginDrawing();
-        ClearBackground(BLACK);
-        const char *stext = TextFormat("%d", (int)score);
-        int sw = MeasureText(stext, 50);
-        DrawText(stext, 1920 - sw - 60, 30, 50, GRAY);
-        DrawRectangle((int)x, (int)y, 40, 40, WHITE);
-        for (int i = 0; i < MAX; i++) {
-            DrawRectangle((int)e[i].x, (int)e[i].y, 40, 40, RED);
-        }
-        EndDrawing();
-    }
+    ResetGame();
+    state = MENU;
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(UpdateDrawFrame, 60, 1);
+#else
+    while (!WindowShouldClose()) UpdateDrawFrame();
+#endif
     UnloadMenuFont();
     CloseWindow();
     return 0;
